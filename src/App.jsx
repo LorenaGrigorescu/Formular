cad5654717ca1bee7db752943aced334dfcb30b4
Grigorescu Lorena <lrngrigorescu@gmail.com>
2023-06-04 12:29:53 +0300

    book appointment profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Register from './components/Register';
 import Login from './components/Login';
+import BookAppointment from './components/BookAppointment';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Profile from './components/Profile';
 
 function App() {
 
   return (
     <Router>
-    <main className="App">
+    <main className="text-primary-emphasis App">
       <Routes>
-          <Route path="/" element={<Register />} />
+          {/* <Route path="/" element={<Register />} /> */}
           <Route path="/login" element={<Login />} />
+          <Route path='/profile' element={<Profile />} />
           {/* Add more routes as needed */}
         </Routes>
     </main>
---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import BookAppointment from "./BookAppointment";
+
+const Profile = () => {
+
+    return(
+        <div className="moveRight">
+            <BookAppointment />
+        </div>
+    );
+}
+
+export default Profile;
