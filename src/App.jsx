9928a0ae2f78171fb3b7f8673ba6ee656304c1a8
AlexandraStefaniaDumitru <dumitrustefania10@yahoo.com>
2023-06-05 11:07:04 +0300

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./components/Login";
 import Children from "./components/Children";
 import ChildPhoto from "./components/ChildPhoto";
 import UserPhoto from "./components/UserPhoto";
+import LandingPage from "./components/LandingPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
     <Router>
       <main className="App">
         <Routes>
-          <Route path="/" element={<Register />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/children" element={<Children />} />
           <Route path="/children/:name" element={<ChildPhoto />} />
